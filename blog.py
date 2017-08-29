29028e0f7d58747b19fa89a329aa8977c0f6b405
Michael Skolnik <mikeskolnik75@gmail.com>
2017-08-28 20:40:22 -0400

    Second commit - Added HTML templates

--- blog.py
+++ blog.py
@@ -19,5 +19,15 @@ def connect_db():
     return sqlite3.connect(app.config["DATABASE"])
 
 
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/main")
+def main():
+    return render_template("main.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
