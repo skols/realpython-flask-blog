fb8352f267b21a8acc877ead72cdb688b6d228a8
Michael Skolnik <mikeskolnik75@gmail.com>
2017-08-28 21:14:26 -0400

    Third commit - User login

--- blog.py
+++ blog.py
@@ -7,6 +7,9 @@ import sqlite3
 
 # configuration
 DATABASE = "blog.db"
+USERNAME = "admin"
+PASSWORD = "admin"
+SECRET_KEY = "b'\x88\x9a\xd6\xb1\xc5\xae\xa1d\x08\xc0\n\x93\x84a\xf8\xb9x~"
 
 app = Flask(__name__)
 
@@ -19,14 +22,31 @@ def connect_db():
     return sqlite3.connect(app.config["DATABASE"])
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    error = None
+    status_code = 200
+    if request.method == "POST":
+        if request.form["username"] != app.config["USERNAME"] or \
+                request.form["password"] != app.config["PASSWORD"]:
+                    error = "Invalid credentials. Please try again."
+                    status_code = 400
+        else:
+            session["logged_in"] = True
+            return redirect(url_for("main"))
+    return render_template("login.html", error=error), status_code
 
 
 @app.route("/main")
 def main():
     return render_template("main.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("logged_in", None)
+    flash("You were logged out")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
